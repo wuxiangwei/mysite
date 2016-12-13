6a6f396d01276297810b4b2c624122a039c67508
wuxiangwei <wuxiangwei@h3c.com>
2016-12-13 01:13:38 -0500

    增加版权声明

---
+++ scripts/add_tail.js
@@ -0,0 +1,15 @@
+// Add a tail to every post from  tail.md
+// Greate for adding copyright info
+
+var fs = require('fs')
+
+hexo.extend.filter.register('before_post_render', function(data){
+    if (data.copyright == false) return data;
+    var file_content = fs.readFileSync('tail.md');
+    if (file_content && data.content.length > 50) {
+        data.content += file_content;
+        // var permalink = '\n本文永久链接：' + data.permalink;
+        // data.content += permalink;
+    }
+    return data;
+});
---
+++ tail.md
@@ -0,0 +1,5 @@
+
+-------------------
+**版权声明**
+
+本作品采用[知识共享署名2.5 中国大陆许可协议](https://creativecommons.org/licenses/by/2.5/cn/)进行许可，欢迎转载，但转载请注明来自[*Shanno's Blog*](https://wuxiangwei.cn/)，并保持转载后文章内容的完整。本人保留所有版权相关权利，侵权必究。
